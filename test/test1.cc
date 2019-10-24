dcb4f929622a4d488ef6f5da849c47ad4fa48c64
huangbn <huangbn@gmail.com>
2019-10-24 16:37:30 +0800

    Added CMake Added GTest Added Namesapce "GUNDAM" Modified some file names

---
+++ test/test1.cc
@@ -0,0 +1,9 @@
+#include "gundam/graph.h"
+
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+TEST(Test1, CheckGoogleTest) { ASSERT_TRUE(true); }
+
+TEST(Test1, TestGraphClass) { ASSERT_TRUE(true); }
