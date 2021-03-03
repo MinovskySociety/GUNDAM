876b50da690ba64ea0a543cb74d088df0bcadef6
fuwenzhi <s2005695@ed.ac.uk>
2021-03-03 21:59:59 +0000

    remove the const reference in SamePattern

--- include/gundam/tool/same_pattern.h
+++ include/gundam/tool/same_pattern.h
@@ -11,18 +11,14 @@ inline bool SamePattern(GraphType0& graph_0,
                         GraphType1& graph_1) {
   // just begin bfs at a random vertex and find whether
   // it can reach all vertexes
-  const auto& graph_0_const_ref = graph_0;
-  const auto& graph_1_const_ref = graph_1;
-
-  if ((graph_0_const_ref.CountVertex()
-    != graph_1_const_ref.CountVertex())
-    ||(graph_0_const_ref.CountEdge()
-    != graph_1_const_ref.CountEdge())) {
+  if ((graph_0.CountVertex()
+    != graph_1.CountVertex())
+    ||(graph_0.CountEdge()
+    != graph_1.CountEdge())) {
     return false;
   }
 
-  auto ret = GUNDAM::DpisoUsingMatch(graph_0_const_ref,
-                                     graph_1_const_ref, 1);
+  auto ret = GUNDAM::DpisoUsingMatch(graph_0, graph_1, 1);
 
   if (ret == 0){
     return false;
