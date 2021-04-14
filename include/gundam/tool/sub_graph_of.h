326370afb6fa32c9fb390bccfa85ccaad2ce9142
fuwenzhi <s2005695@ed.ac.uk>
2021-04-14 22:04:36 +0100

    allow the partial match to be configured in SubGraphOf, upload progress of MultiQueryDpIso

--- include/gundam/tool/sub_graph_of.h
+++ include/gundam/tool/sub_graph_of.h
@@ -1,6 +1,7 @@
 #ifndef _GUNDAM_SUB_GRAPH_OF_H
 #define _GUNDAM_SUB_GRAPH_OF_H
 
+#include "gundam/match/match.h"
 #include "gundam/algorithm/dp_iso_using_match.h"
 
 namespace GUNDAM {
@@ -9,7 +10,9 @@ namespace GUNDAM {
 template <typename GraphType0,
           typename GraphType1>
 inline bool SubGraphOf(GraphType0& graph_0,
-                       GraphType1& graph_1) {
+                       GraphType1& graph_1,                       
+                 Match<GraphType0,
+                       GraphType1>& match_graph_0_to_graph_1) {
   // just begin bfs at a random vertex and find whether
   // it can reach all vertexes
   if ((graph_0.CountVertex() > graph_1.CountVertex())
@@ -17,12 +20,20 @@ inline bool SubGraphOf(GraphType0& graph_0,
     return false;
   }
 
-  auto ret = GUNDAM::DpisoUsingMatch(graph_0, graph_1, 1);
+  auto ret = GUNDAM::DpisoUsingMatch(graph_0, graph_1, match_graph_0_to_graph_1, 1);
   assert(ret == 0 || ret == 1);
   if (ret == 0){
     return false;
   }
   return true;
+}
+
+template <typename GraphType0,
+          typename GraphType1>
+inline bool SubGraphOf(GraphType0& graph_0,
+                       GraphType1& graph_1) {
+  Match<GraphType0, GraphType1> match_graph_0_to_graph_1;
+  return SubGraphOf(graph_0, graph_1, match_graph_0_to_graph_1);
 }
 
 }  // namespace GUNDAM
