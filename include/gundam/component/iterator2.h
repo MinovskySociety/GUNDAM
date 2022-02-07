bec3a937b473c77d84264e6a41cfddb16c6ca3bf
fuwenzhi <fuwenzhi@buaa.edu.cn>
2022-02-07 15:47:59 +0000

    modified operator== & operator!= in iterator2.h

--- include/gundam/component/iterator2.h
+++ include/gundam/component/iterator2.h
@@ -53,6 +53,10 @@ template <class IteratorType, class ValueType,
           template <class _IteratorType, class _ValueType> class Cast =
               DefaultCast>
 class GIterator {
+ private:
+  template <class, class, template <class, class> class>
+  friend class GIterator;
+
  public:
   GIterator() = default;
 
@@ -76,6 +80,23 @@ class GIterator {
 
   operator const ValueType *() const {
     return &Cast<IteratorType, ValueType>()(it_);
+  }
+
+  bool operator==(const GIterator &b) const {
+    assert(graph_);
+    assert(b.graph_);
+    assert(end_ == b.end_);
+    return it_ == b.it_;
+  }
+
+  template <class _IteratorType, 
+            class _ValueType, 
+            template <class, class> class _Cast>
+  bool operator==(const GIterator<_IteratorType,
+                                  _ValueType,
+                                  _Cast> &b) const {
+    assert(end_ == b.end_);
+    return it_ == b.it_;
   }
 
   GIterator &operator++() {
@@ -104,6 +125,10 @@ class GIterator {
 template <bool is_const, class GraphType, class IteratorType, class ValueType,
           class PointerType>
 class GIterator2 {
+ private:
+  template <bool, class, class, class, class>
+  friend class GIterator2;
+
  public:
   GIterator2() : graph_(nullptr),
                  it_(), end_(it_){
@@ -159,6 +184,22 @@ class GIterator2 {
   }
 
   bool operator==(const GIterator2 &b) const {
+    assert(graph_);
+    assert(b.graph_);
+    assert(end_ == b.end_);
+    return it_ == b.it_;
+  }
+
+  template <bool _is_const, 
+            class _GraphType, 
+            class _IteratorType, 
+            class _ValueType,
+            class _PointerType>
+  bool operator==(const GIterator2<_is_const,
+                                   _GraphType,
+                                   _IteratorType,
+                                   _ValueType,
+                                   _PointerType> &b) const {
     assert(graph_);
     assert(b.graph_);
     assert(end_ == b.end_);
