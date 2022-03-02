9dfcac42288326621e97ccef82aca144b8e9c6f4
muyangliu <muyangliu@buaa.edu.cn>
2022-03-02 21:04:05 +0000

    seperate differents CCs

--- test/test_connected_component.cc
+++ test/test_connected_component.cc
@@ -4,6 +4,7 @@
 #include "gtest/gtest.h"
 
 #include "gundam/tool/connected_component.h"
+#include "gundam/tool/topological/star/is_star.h"
 
 #include "gundam/graph_type/graph_base.h"
 #include "gundam/graph_type/small_graph.h"
@@ -13,7 +14,239 @@
 
 template<class GraphType>
 void TestConnectedComponent(){
-  ASSERT_TRUE(false);
+
+  GraphType g0;
+
+  std::cout << "GraphType: " << typeid(g0).name() << std::endl;
+
+  // 1 -> 2 -> 3
+  // /\        |
+  // |         V
+  // 8         4
+  // /\        |
+  // |         V
+  // 7 <- 6 <- 5
+
+
+  // AddVertex
+  auto res1 = g0.AddVertex(1, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g0.AddVertex(2, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g0.AddVertex(3, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g0.AddVertex(4, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g0.AddVertex(5, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g0.AddVertex(6, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g0.AddVertex(7, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g0.AddVertex(8, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+
+  // AddEdge
+  auto res2 = g0.AddEdge(1, 2, 42, 1);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g0.AddEdge(2, 3, 42, 2);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g0.AddEdge(3, 4, 42, 3);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g0.AddEdge(4, 5, 42, 4);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g0.AddEdge(5, 6, 42, 5);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g0.AddEdge(6, 7, 42, 6);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g0.AddEdge(7, 8, 42, 7);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g0.AddEdge(8, 1, 42, 8);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+
+  auto components0 = GUNDAM::ConnectedComponent(g0);
+  ASSERT_EQ(components0.size(), 1);
+  ASSERT_EQ(components0[0].CountVertex(), 8);
+  ASSERT_EQ(components0[0].CountEdge(), 8);
+
+
+  // 1 -> 2 -> 3   0
+  // /\        |
+  // |         V
+  // 8         4
+  // /\        |
+  // |         V
+  // 7 <- 6 <- 5
+
+
+
+  res1 = g0.AddVertex(0, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+
+
+  auto components1 = GUNDAM::ConnectedComponent(g0);
+  ASSERT_EQ(components1.size(), 2);
+  ASSERT_EQ(components1[0].CountVertex(), 1);
+  ASSERT_EQ(components1[1].CountVertex(), 8);
+  ASSERT_EQ(components1[0].CountEdge(), 0);
+  ASSERT_EQ(components1[1].CountEdge(), 8);
+
+
+  // 1 -> 2    5 -> 6
+  // /\   |    /\   |
+  // |    V    |    V
+  // 4 <- 3    8 <- 7
+
+  GraphType g2;
+
+  // AddVertex
+  res1 = g2.AddVertex(1, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g2.AddVertex(2, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g2.AddVertex(3, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g2.AddVertex(4, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g2.AddVertex(5, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g2.AddVertex(6, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g2.AddVertex(7, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g2.AddVertex(8, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+
+  // AddEdge
+  res2 = g2.AddEdge(1, 2, 42, 1);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g2.AddEdge(2, 3, 42, 2);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g2.AddEdge(3, 4, 42, 3);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g2.AddEdge(4, 1, 42, 4);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+
+  res2 = g2.AddEdge(5, 6, 42, 5);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g2.AddEdge(6, 7, 42, 6);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g2.AddEdge(7, 8, 42, 7);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g2.AddEdge(8, 5, 42, 8);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+
+
+  auto components2 = GUNDAM::ConnectedComponent(g2);
+  ASSERT_EQ(components2.size(), 2);
+  ASSERT_EQ(components2[0].CountVertex(), 4);
+  ASSERT_EQ(components2[1].CountVertex(), 4);
+  ASSERT_EQ(components2[0].CountEdge(), 4);
+  ASSERT_EQ(components2[1].CountEdge(), 4);
+
+//  1 -> 2 -> 3    5 <- 6 -< 8 
+//       |              |
+//       \/             \/
+//       4              7 -> 9
+
+  GraphType g3;
+
+  res1 = g3.AddVertex(1, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g3.AddVertex(2, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g3.AddVertex(3, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g3.AddVertex(4, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g3.AddVertex(5, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g3.AddVertex(6, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g3.AddVertex(7, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g3.AddVertex(8, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g3.AddVertex(9, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+
+  res2 = g3.AddEdge(1, 2, 42 ,1);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g3.AddEdge(2, 3, 42 ,2);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g3.AddEdge(2, 4, 42 ,3);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g3.AddEdge(6, 5, 42 ,4);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g3.AddEdge(6, 7, 42 ,5);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g3.AddEdge(7, 9, 42 ,6);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g3.AddEdge(8, 6, 42 ,7);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+
+
+  auto components3 = GUNDAM::ConnectedComponent(g3);
+  ASSERT_EQ(components3.size(), 2);
+  ASSERT_EQ(components3[0].CountVertex(), 4);
+  ASSERT_EQ(components3[1].CountVertex(), 5);
+  ASSERT_EQ(components3[0].CountEdge(), 3);
+  ASSERT_EQ(components3[1].CountEdge(), 4);
+
+  bool is_star1 = GUNDAM::IsStar<true>(components3[0]);
+  ASSERT_TRUE(is_star1);
+  bool is_star2 = GUNDAM::IsStar<true>(components3[1]);
+  ASSERT_TRUE(is_star2);
+
   return;
 }
 
