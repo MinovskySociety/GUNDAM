2e9d6ad7397a9d967b29f11a233fa0e74279c14e
muyangliu <muyangliu@buaa.edu.cn>
2022-03-10 18:32:38 +0000

    merge graph, tested with small cases

--- include/gundam/tool/operator/merge_graph.h
+++ include/gundam/tool/operator/merge_graph.h
@@ -1,6 +1,16 @@
 #ifndef _GUNDAM_TOOL_OPERATOR_MERGE_GRAPH_SET_H
 #define _GUNDAM_TOOL_OPERATOR_MERGE_GRAPH_SET_H
 
+#include "gundam/type_getter/edge_handle.h"
+#include "gundam/type_getter/vertex_handle.h"
+#include "gundam/type_getter/graph_parameter_getter.h"
+#include "gundam/tool/max_id.h"
+
+#include <vector>
+#include <set>
+#include <map>
+#include <iostream>
+
 namespace GUNDAM {
 
 /* ##################################################### *
@@ -10,30 +20,150 @@ namespace GUNDAM {
 template <typename GraphType>
 inline std::remove_cv_t<
        std::remove_reference_t<GraphType>> 
-       MergeGraphSet(const std::vector<GraphType>& input_graph_set,
+       MergeGraphSet(const std::vector<GraphType>& input_graph_vec,
                      const std::vector<
-                     typename VertexID<GraphType>::type>& join_vertex_id_set
-                   = const std::vector<
+                     typename VertexID<GraphType>::type>& join_vertex_id_vec
+                   = std::vector<
                      typename VertexID<GraphType>::type>() ) {
 
   using ReturnGraphType = std::remove_cv_t<
                    std::remove_reference_t<GraphType>>;
+  using VertexIDType = typename VertexID<GraphType>::type;
+  using EdgeIDType = typename EdgeID<GraphType>::type;
+  using   EdgeHandleType = typename GUNDAM::  EdgeHandle<GraphType>::type;
+  using VertexHandleType = typename GUNDAM::VertexHandle<GraphType>::type;
+  using VertexConstHandleType = typename GUNDAM::VertexHandle<const GraphType>::type;
+
 
   #ifndef NDEBUG
-  for (const auto& join_vertex_id : join_vertex_id_set) {
-    for (const auto& input_graph : input_graph_set) {
+  for (const auto& join_vertex_id : join_vertex_id_vec) {
+    for (const auto& input_graph : input_graph_vec) {
       // should be contained in all input graphs
       assert(input_graph.FindVertex(join_vertex_id));
     }
   }
   #endif // NDEBUG
-  
+
   ReturnGraphType merged_graph;
 
-  /* ######################### *
-   * ##  do something here  ## *
-   * ######################### */
- 
+  std::set<VertexIDType> join_vertex_id_set;
+  for(const auto & join_vertex_id : join_vertex_id_vec) {
+    join_vertex_id_set.insert(join_vertex_id);
+  }
+
+
+  for (const auto & input_graph : input_graph_vec) {
+    std::map<VertexIDType, VertexHandleType> vid2handle;
+    for (auto vertex_it = input_graph.VertexBegin();
+              !vertex_it.IsDone();
+              vertex_it++) {
+      if (join_vertex_id_set.find(vertex_it->id()) == join_vertex_id_set.end()) {
+        vid2handle[vertex_it->id()] = VertexHandleType();
+      } else {
+        vid2handle[vertex_it->id()] = merged_graph.FindVertex(vertex_it->id());
+      }
+    }
+    
+    for (auto vertex_it = input_graph.VertexBegin();
+              !vertex_it.IsDone();
+              vertex_it++) {
+      VertexIDType merged_src_id;
+
+      if (!(vid2handle[vertex_it->id()])) {
+        merged_src_id = (vid2handle[vertex_it->id()])->id();
+      } else {
+        if (join_vertex_id_set.find(vertex_it->id()) != join_vertex_id_set.end()) {
+          merged_src_id = vertex_it->id();
+        } else {
+          merged_src_id = MaxVertexId<GraphType>(merged_graph) + 1;
+
+          while (join_vertex_id_set.find(merged_src_id) != join_vertex_id_set.end()) {
+            merged_src_id++;
+          }
+        }
+
+        auto [merged_src_handle, merged_src_ret] = merged_graph.AddVertex(merged_src_id, vertex_it->label());
+        vid2handle[vertex_it->id()] = merged_src_handle;
+        if (!merged_src_ret) {
+          std::cout << "error inserting srv" << std::endl;
+        }
+
+        if constexpr (GUNDAM::GraphParameter<GraphType>::vertex_has_attribute) {
+          // add attribute to new_vertex
+          for (auto attr_it = vertex_it->AttributeBegin();
+                    !attr_it.IsDone();
+                    attr_it++){
+            auto [attr_handle, ret] = merged_src_handle->AddAttribute(
+                                            attr_it->key(),
+                                            attr_it->value_type(),
+                                            attr_it->value_str());
+          }
+        }
+      }
+
+      for (auto out_edge_it = vertex_it->OutEdgeBegin();
+                !out_edge_it.IsDone();
+                out_edge_it++) {
+        VertexIDType merged_dst_id;
+//        auto dst_handle = out_edge_it->dst_handle();
+        VertexConstHandleType dst_handle = out_edge_it->dst_handle();
+
+        if (!(vid2handle[dst_handle->id()])) {
+          merged_dst_id = (vid2handle[dst_handle->id()])->id();
+        } else {
+          if (join_vertex_id_set.find(dst_handle->id()) != join_vertex_id_set.end()) {
+            merged_dst_id = vertex_it->id();
+          } else {
+            merged_dst_id = MaxVertexId<GraphType>(merged_graph) + 1;
+
+            while (join_vertex_id_set.find(merged_dst_id) != join_vertex_id_set.end()) {
+              merged_dst_id++;
+            }
+          }
+
+          auto [merged_dst_handle, merged_dst_ret] = merged_graph.AddVertex(merged_dst_id, dst_handle->label());
+          vid2handle[dst_handle->id()] = merged_dst_handle;
+          if (!merged_dst_ret) {
+            std::cout << "error inserting dst" << std::endl;
+          }
+
+          if constexpr (GUNDAM::GraphParameter<GraphType>::vertex_has_attribute) {
+            // add attribute to new_vertex
+            for (auto attr_it = dst_handle->AttributeBegin();
+                      !attr_it.IsDone();
+                      attr_it++){
+              auto [attr_handle, ret] = merged_dst_handle->AddAttribute(
+                                              attr_it->key(),
+                                              attr_it->value_type(),
+                                              attr_it->value_str());
+            }
+          }
+        }
+
+        EdgeIDType merged_edge_id = MaxEdgeId<GraphType>(merged_graph) + 1;
+        auto [merged_edge_handle, merged_edge_ret] = merged_graph.AddEdge(merged_src_id,
+                                                        merged_dst_id,
+                                                        out_edge_it->label(),
+                                                        merged_edge_id);
+        if (!merged_edge_ret) {
+          std::cout << "error inserting edge" << std::endl;
+        }
+
+        if constexpr (GUNDAM::GraphParameter<GraphType>::edge_has_attribute) {
+          for (auto attr_it = out_edge_it->AttributeBegin();
+                   !attr_it.IsDone();
+                    attr_it++){
+            auto [attr_handle, ret] = merged_edge_handle->AddAttribute(
+                                            attr_it->key(),
+                                            attr_it->value_type(),
+                                            attr_it->value_str());
+          }
+        }
+      }
+    }
+  }
+
+
   #ifndef NDEBUG
   for (const auto& join_vertex_id : join_vertex_id_set) {
     // should also be contained in the merged graph
@@ -42,5 +172,5 @@ inline std::remove_cv_t<
   #endif // NDEBUG
   return merged_graph;
 };
-
+}
 #endif // _GUNDAM_TOOL_OPERATOR_MERGE_GRAPH_SET_H
---
+++ test/test_merge_graph.cc
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "gundam/tool/operator/merge_graph.h"
+
+#include "gundam/graph_type/graph_base.h"
+#include "gundam/graph_type/small_graph.h"
+#include "gundam/graph_type/large_graph.h"
+#include "gundam/graph_type/large_graph2.h"
+#include "gundam/graph_type/graph.h"
+#include "gundam/tool/max_id.h"
+
+template<class GraphType>
+void TestMergeGraphs(){
+ 
+  using VertexIDType = typename GraphType::VertexType::IDType;
+
+  GraphType g0;
+
+  // 1 -> 2 -> 3
+
+
+  auto res1 = g0.AddVertex(1, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g0.AddVertex(2, 1);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g0.AddVertex(3, 2);
+
+  // AddEdge
+  auto res2 = g0.AddEdge(1, 2, 42, 1);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g0.AddEdge(2, 3, 42, 2);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+
+  GraphType g1;
+
+  // 1 -> 5 -> 3
+
+
+  res1 = g1.AddVertex(1, 0);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g1.AddVertex(5, 1);
+  ASSERT_TRUE(res1.first);
+  ASSERT_TRUE(res1.second);
+  res1 = g1.AddVertex(3, 2);
+
+  // AddEdge
+  res2 = g1.AddEdge(1, 5, 42, 1);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+  res2 = g1.AddEdge(5, 3, 42, 2);
+  ASSERT_TRUE(res2.first);
+  ASSERT_TRUE(res2.second);
+
+  std::vector<GraphType> path_vec;
+  path_vec.push_back(g0);
+  path_vec.push_back(g1);
+
+  std::vector<VertexIDType> id_vec;
+  
+  GraphType mg0 = GUNDAM::MergeGraphSet(path_vec, id_vec);
+  ASSERT_EQ(mg0.CountVertex(), 6);
+  id_vec.push_back(1);
+
+  GraphType mg1 = GUNDAM::MergeGraphSet(path_vec, id_vec);
+
+  ASSERT_EQ(mg1.CountVertex(), 5);
+
+  return;
+}
+
+TEST(TestGUNDAM, TestMergeGraphs) {
+  using namespace GUNDAM;
+
+  using G1 = LargeGraph<uint32_t, uint32_t, std::string, 
+                        uint64_t, uint32_t, std::string>;
+
+  using G2 = LargeGraph2<uint32_t, uint32_t, std::string, 
+                         uint64_t, uint32_t, std::string>;  
+
+  using G3 = SmallGraph<uint32_t, uint32_t, 
+                        uint64_t, uint32_t>;
+
+  using G4 = Graph<SetVertexIDType<uint32_t>, 
+                   SetVertexLabelType<uint32_t>,
+                   SetVertexAttributeKeyType<std::string>, 
+                   SetEdgeIDType<uint64_t>,
+                   SetEdgeLabelType<uint32_t>, 
+                   SetEdgeAttributeKeyType<std::string>>;
+
+  using G5 = Graph<SetVertexIDType<uint32_t>, 
+                   SetVertexLabelType<uint32_t>,
+                   SetVertexAttributeKeyType<std::string>,
+                   SetEdgeIDType<uint64_t>,
+                   SetEdgeLabelType<uint32_t>, 
+                   SetEdgeAttributeKeyType<std::string>, 
+                   SetVertexLabelContainerType<GUNDAM::ContainerType::Map>,
+                   SetVertexIDContainerType<GUNDAM::ContainerType::Map>>;
+
+  using G6 = Graph<SetVertexIDType<uint32_t>, 
+                   SetVertexLabelType<uint32_t>,
+                   SetVertexAttributeKeyType<std::string>,
+                   SetEdgeIDType<uint64_t>,
+                   SetEdgeLabelType<uint32_t>, 
+                   SetEdgeAttributeKeyType<std::string>, 
+                   SetVertexPtrContainerType<GUNDAM::ContainerType::Map>,
+                   SetEdgeLabelContainerType<GUNDAM::ContainerType::Map>>;
+
+  using G7 = Graph<SetVertexIDType<uint32_t>, 
+                   SetVertexLabelType<uint32_t>,
+                   SetVertexAttributeKeyType<std::string>,
+                   SetEdgeIDType<uint64_t>,
+                   SetEdgeLabelType<uint32_t>, 
+                   SetEdgeAttributeKeyType<std::string>, 
+                   SetVertexLabelContainerType<GUNDAM::ContainerType::Map>,
+                   SetVertexIDContainerType<GUNDAM::ContainerType::Map>, 
+                   SetVertexPtrContainerType<GUNDAM::ContainerType::Map>,
+                   SetEdgeLabelContainerType<GUNDAM::ContainerType::Map>>;
+
+  TestMergeGraphs<G1>();
+  TestMergeGraphs<G2>();
+  TestMergeGraphs<G3>();
+  TestMergeGraphs<G4>();
+  TestMergeGraphs<G5>();
+  TestMergeGraphs<G6>();
+  TestMergeGraphs<G7>();
+}
