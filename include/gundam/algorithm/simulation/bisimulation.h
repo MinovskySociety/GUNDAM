1fd6fc7007bd176dec2f8012223bb9ccde617457
fuwenzhi <s2005695@ed.ac.uk>
2022-05-07 21:55:01 +0100

    update

--- include/gundam/algorithm/simulation/bisimulation.h
+++ include/gundam/algorithm/simulation/bisimulation.h
@@ -2,6 +2,8 @@
 #define _GUNDAM_ALGORITHM_SIMULATION_BISIMULATION_H
 
 #include "gundam/tool/vertex_degree_filter.h"
+#include "gundam/tool/topological/dag/is_acyclic.h"
+#include "gundam/tool/operator/merge_vertex.h"
 
 #include "gundam/match/match.h"
 
@@ -25,6 +27,8 @@ inline void BisimulationGeneralCase(GraphType& graph,
 
   auto strong_connected_component_vertex_set
      = StrongConnectedComponent(graph);
+
+  assert(IsAcyclic(MergeVertex(graph, strong_connected_component_vertex_set)));
 
   /* ################################################ *
    * ##  calc leaf_handle_set_in_g_scc             ## *
@@ -107,8 +111,61 @@ inline void BisimulationGeneralCase(GraphType& graph,
     assert(std::numeric_limits<RankType>::min() < 0);
   }
 
-  // calculate the rank of each vertex
-  std::vector<VertexHandleType> leaf_handle_set_in_g_scc_not_in_g;
+  /* ################################################ *
+   * ##  calc the WF                               ## *
+   * ##     WF(G) = {n ∈ N : G(n) is acyclic}.     ## *
+   * ##     G(n) is the reachable subgraph from n  ## *
+   * ################################################ */
+  std::vector<VertexHandleType> vertexes_in_scc_with_cycle;
+  std::unordered_set<VertexIDType> not_in_wf;
+  for (auto& strong_connected_component_vertex
+           : strong_connected_component_vertex_set) {
+    assert(!strong_connected_component_vertex.empty());
+    if (strong_connected_component_vertex.size() == 1) {
+      continue;
+    }
+    for (const auto& vertex_handle : strong_connected_component_vertex) {
+      not_in_wf.emplace(vertex_handle->id());
+    }
+    // such a subgraph contains loop, iterate over
+    // input edges from 
+    std::move(strong_connected_component_vertex.begin(), 
+              strong_connected_component_vertex.end(), 
+              std::back_inserter(vertexes_in_scc_with_cycle));
+  }
+
+  std::queue<VertexHandleType, 
+  std::deque<VertexHandleType>> queue_of_vertex_in_scc_with_cycle(
+  std::deque<VertexHandleType>(vertexes_in_scc_with_cycle.begin(),
+                               vertexes_in_scc_with_cycle.end()));
+
+  assert(not_in_wf_it.size() == queue_of_vertex_in_scc_with_cycle.size());
+  while (!queue_of_vertex_in_scc_with_cycle.empty()) {
+    auto vertex_handle = queue_of_vertex_in_scc_with_cycle.front();
+    assert(not_in_wf.find(vertex_handle->id())
+        != not_in_wf.end ());
+    queue_of_vertex_in_scc_with_cycle.pop();
+    for (auto in_edge_it = vertex_handle.InEdgeBegin();
+             !in_edge_it.IsDone();
+              in_edge_it++) {
+      auto not_in_wf_it  = not_in_wf.find(in_edge_it->src_handle()->id());
+      if ( not_in_wf_it != not_in_wf.end()) {
+        // has been considered
+        continue;
+      }
+      not_in_wf.emplace_hint(not_in_wf_it, in_edge_it->src_handle()->id());
+      queue_of_vertex_in_scc_with_cycle.emplace_back(in_edge_it->src_handle());
+    }
+  }
+
+  std::queue<VertexHandleType, 
+  std::deque<VertexHandleType>> queue_of_vertex(
+  std::deque<VertexHandleType>(leaf_handle_set_in_g.begin(),
+                               leaf_handle_set_in_g.end()));
+
+  while (!queue_of_vertex.empty()) {
+
+  }
   return;
 }
 
