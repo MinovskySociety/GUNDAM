6303d78cf31e6578aa115bb793b1de66a893213b
Wenzhi Fu <s2005695@hendry.inf.ed.ac.uk>
2023-06-27 21:14:23 +0100

    bug fixed in reset_edge_label

--- include/gundam/tool/operator/reset_edge_label.h
+++ include/gundam/tool/operator/reset_edge_label.h
@@ -24,7 +24,13 @@ GraphType ResetEdgeLabel(const GraphType& graph,
               out_edge_it++) {
       auto edge_label_map_it = edge_label_map.find(std::pair(out_edge_it->src_handle()->label(),
                                                              out_edge_it->dst_handle()->label()));
-      assert(edge_label_map_it != edge_label_map.end());
+      if (edge_label_map_it == edge_label_map.end()) {
+        new_graph.AddEdge(out_edge_it->src_handle()->id(),
+                          out_edge_it->dst_handle()->id(),
+                          out_edge_it->label(),
+                          out_edge_it->id());
+        continue;
+      }
       new_graph.AddEdge(out_edge_it->src_handle()->id(),
                         out_edge_it->dst_handle()->id(),
                         edge_label_map_it->second,
